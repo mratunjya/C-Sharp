0b91be9b75a1482b4331c68eca3a1fabc56daede
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2020-03-22 10:07:16 +0530

    Hello World
    This is the Very First Program of C#.
    
    Every Developer must write a Hello World as there starting Program 😎.

---
+++ 1.  Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.__Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
